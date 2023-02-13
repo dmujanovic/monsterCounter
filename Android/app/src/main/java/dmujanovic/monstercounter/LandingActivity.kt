35e7776630d3b703f315897df2266570032ea440
Dominik Mujanović <dmujanovi@foi.hr>
2023-02-13 13:48:37 +0100

    resursi i glavna aktivnost
    dodani resursi za projekt i glavna aktivnost.

--- Android/app/src/main/java/dmujanovic/monstercounter/LandingActivity.kt
+++ Android/app/src/main/java/dmujanovic/monstercounter/LandingActivity.kt
@@ -1,11 +1,19 @@
 package dmujanovic.monstercounter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        val btnMain = findViewById<Button>(R.id.btnPlay)
+        btnMain.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Android/app/src/main/java/dmujanovic/monstercounter/MainActivity.kt
+++ Android/app/src/main/java/dmujanovic/monstercounter/MainActivity.kt
@@ -2,10 +2,38 @@ package dmujanovic.monstercounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var scoreCounter = 0
+        var monster = 1
+        var progressCounter = 0
+        val pointsDisplay = findViewById<TextView>(R.id.textViewMaximumPoints)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBarMonster)
+
+
+        val monsterImage = findViewById<ImageView>(R.id.imageViewMonster)
+        monsterImage.setOnClickListener {
+            if(scoreCounter<100){
+                if (monster == 1) {
+                    scoreCounter += 10
+                    progressCounter += 10
+                    pointsDisplay.text = (100 - scoreCounter.toString().toInt()).toString()
+                    progressBar.progress = progressCounter
+                }
+            }
+            if(scoreCounter==100){
+                pointsDisplay.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0,0)
+                pointsDisplay.text = "Claim"
+            }
+
+        }
     }
 }
