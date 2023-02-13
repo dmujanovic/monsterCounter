15ab0d159c45731062ee225bccbf069b733184a2
Dominik Mujanović <dmujanovi@foi.hr>
2023-02-13 10:05:13 +0100

    osnovni dizajn
    splash screen, landing page, app logo, maknut action bar, custom font.

---
+++ Android/app/src/main/java/dmujanovic/monstercounter/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package dmujanovic.monstercounter
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        SplashLogo.alpha = 0f
+        SplashLogo.animate().setDuration(2000).alpha(1f).withEndAction{
+            val intent = Intent(this,LandingActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
